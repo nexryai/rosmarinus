53d972105493f42e85130c15d810ac47b7320fc0
nexryai <nexryai@sda1.net>
2026-07-19 05:04:54 +0000

    test: Fix log

--- test/federation/misskey_test.go
+++ test/federation/misskey_test.go
@@ -92,7 +92,7 @@ func TestLatestMisskeyFollowAcceptAndNoteDelivery(t *testing.T) {
 			return false
 		}
 		relationship, findErr = followRepo.Find(ctx, localActor.ID, remoteActor.ID)
-		t.Logf("[DEBUG] followRepo.Find: %s <= %s: %d", localActor.ID, remoteActor.ID, &relationship)
+		t.Logf("[DEBUG] followRepo.Find: %s <= %s: %v", localActor.ID, remoteActor.ID, &relationship)
 		return findErr == nil && relationship != nil && relationship.Status == follows.StatusAccepted
 	})
 
@@ -112,7 +112,7 @@ func TestLatestMisskeyFollowAcceptAndNoteDelivery(t *testing.T) {
 	noteURI := "https://a.test/notes/" + created.CreatedNote.ID
 	waitFor(t, ctx, "Create(Note) stored by Rosmarinus", func() bool {
 		note, findErr := noteRepo.FindByURI(ctx, noteURI)
-		t.Logf("[DEBUG] noteRepo.FindByURI: %s: %d", noteURI, &note)
+		t.Logf("[DEBUG] noteRepo.FindByURI: %s: %s", noteURI, note.Text)
 		return findErr == nil && note != nil && note.Text == "Hello from latest Misskey federation test"
 	})
 }
